d58caca5e1a1b5a9a78686d82d947ab4c99aea59
Billy Korando <bkorando@keyholesoftware.com>
2017-12-10 22:41:08 -0600

    Polish

--- acceptance-tests/src/test/java/com/ps/bk/hotel/test/steps/ExistingCustomerBooksRoom.java
+++ acceptance-tests/src/test/java/com/ps/bk/hotel/test/steps/ExistingCustomerBooksRoom.java
@@ -18,7 +18,6 @@ import com.ps.bk.hotel.booking.model.Booking;
 import com.ps.bk.hotel.customer.model.Customer;
 import com.ps.bk.hotel.room.model.Room;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
